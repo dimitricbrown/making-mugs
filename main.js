67e045495e97cc8432633a7c597c091bb2b65fd4
dimitricbrown <dimitricbrown@outlook.com>
2022-11-21 06:16:36 -0600

    Add final code

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (const clays of clay) {
+   const mug = `${clays} coffee mug`
+   toFireInKiln.push(mug)
+}
+
+console.log(toFireInKiln)
